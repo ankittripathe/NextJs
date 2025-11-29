a594391af09976120957461717b43ef279c8a7b5
Ankit Tripathi <ankittripathi2k24@gmail.com>
2025-11-30 00:34:28 +0530

    nested routing

---
+++ 04_nestedRouting/app/layout.js
@@ -0,0 +1,18 @@
+import Navbar from "./Components/Navbar";
+import "./globals.css";
+
+export const metadata = {
+  title: "Nested Routing",
+  description: "Nested Routing in Nextjs",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
