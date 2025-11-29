99125215016f9cfd47434cdb0f21508fbddb575c
Ankit Tripathi <ankittripathi2k24@gmail.com>
2025-11-29 12:59:17 +0530

    routing in nextjs

--- 02_routing/app/Components/Navbar.jsx
+++ 02_routing/app/Components/Navbar.jsx
@@ -3,14 +3,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center px-10 py-3 mb-5 text-2xl bg-emerald-800">
+    <div className="flex justify-between items-center px-10 py-2 mb-5 text-xl bg-emerald-800">
       <h3>Sheriyans</h3>
 
-      <div className="flex gap-18">
+      <div className="flex gap-12">
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
-        <Link href="/product">Product</Link>
         <Link href="/contact">Contact</Link>
+        <Link href="/product">Product</Link>
+        <Link href="/courses">Courses</Link>
       </div>
     </div>
   );
---
+++ 02_routing/app/courses/[id]/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Dynamic Routing: XYZ Courese</h1>
+    </div>
+  );
+};
+
+export default page;
+
+// Notes:- Dynamic Routing me [] bracket ke ander folder bana loo. Aur phir url me kuch vi likhenge access ho jaega
